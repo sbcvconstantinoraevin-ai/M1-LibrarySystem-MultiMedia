70bc18ec65407f527ba6c302c41fd8b64df3e662
Juhn Lie <sidmanaojuhnlie30@gmail.com>
2026-04-06 22:05:11 +0800

    Add files via upload

---
+++ dist/run.bat
@@ -0,0 +1,2 @@
+@echo off
+java -cp "LibrarySystem.jar;libs\sqlite-jdbc.jar;libs\slf4j-api.jar;libs\slf4j-nop.jar" com.library.Main
---
+++ dist/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Run the Library System
+java -cp "LibrarySystem.jar:libs/sqlite-jdbc.jar:libs/slf4j-api.jar:libs/slf4j-nop.jar" com.library.Main
